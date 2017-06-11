c79bb90a3572603fbf3a34c87492d3138ca18e21
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-06-11 16:24:56 -0300

    melhora da saida de traducoes

--- tradutor.rb
+++ tradutor.rb
@@ -14,7 +14,8 @@ def menu()
   puts 'Escolha uma opção:'
   puts '1 - Adicionar tradução'
   puts '2 - Ver todas as traduções'
-  puts '3 - Buscar uma tradução'
+  puts '3 - Buscar uma tradução para Português'
+  puts '4 - Buscar uma tradução para Inglês'
   puts '0 - sair'
   puts '#' * 20
   puts
@@ -41,36 +42,43 @@ while opcao_escolhida != 0 do
     puts "Escreva a expressão em Ingles: "
     texto_ingles = recebe_texto()
 
-    traducoes << "A tradução de " + texto_portugues + " é " + texto_ingles
+    traducao = { portugues: texto_portugues, ingles: texto_ingles}
+    traducoes << traducao
 
     puts 'Obrigada por inserir mais uma tradução :)'
 
   elsif (opcao_escolhida == 2)
-    puts traducoes
 
+    traducoes.each do |traducao|
+       puts "A tradução de #{traducao[:portugues]} é #{traducao[:ingles]}"
+    end
   elsif (opcao_escolhida == 3)
+    # traducao de ingles para portugues
     puts 'Informe a palavra que deseja procurar: '
     texto_busca = recebe_texto()
 
     traducoes_encontradas = traducoes.select do |traducao|
-       traducao.upcase.include? texto_busca.upcase
+      #puts "#{traducao[:portugues].upcase} -> #{texto_busca.upcase}"
+      traducao[:ingles].upcase.include? texto_busca.upcase
      end
 
-        puts "Nenhuma tradução encontrada" if traducoes_encontradas.empty?
-        puts traducoes_encontradas
+     puts "Nenhuma tradução encontrada" if traducoes_encontradas.empty?
+     puts traducoes_encontradas
+     #imprimir de forma bonita
 
   elsif (opcao_escolhida == 4)
+    # traducao de portugues para ingles
+
     puts 'Informe a palavra que deseja procurar: '
     texto_busca = recebe_texto()
 
     traducoes_encontradas = traducoes.select do |traducao|
-      traducao.upcase.include? texto_busca.upcase
-      end
-
-      puts "Nenhuma tradução encontrada" if traducoes_encontradas.empty?
-      puts traducoes_encontradas
+      #puts "#{traducao[:ingles].upcase} -> #{texto_busca.upcase}"
+      traducao[:portugues].upcase.include? texto_busca.upcase
+    end
+    puts "Nenhuma tradução encontrada" if traducoes_encontradas.empty?
+    puts traducoes_encontradas
   end
-
   menu()
   opcao_escolhida = gets.to_i
 end # while
