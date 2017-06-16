cb2137f522ddbb733fc0b9e987d41c4db6bab078
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-06-16 16:48:41 -0300

    Define o método lista e define todas as traducoes encontradas em lista

--- tradutor_rails/app/controllers/traducoes_controller.rb
+++ tradutor_rails/app/controllers/traducoes_controller.rb
@@ -1,8 +1,9 @@
+#Adiciona a classe Traducoes.controller para a aplicação.
+#Herda a classe ApplicationController, ou seja, a classe que eu criei torna-se uma extensão da classe ApplicationController, que o rails considera como padrão
+
 class TraducoesController < ApplicationController
+  #Define o método lista e define todas as traducoes encontradas em lista
  def lista
    @traducoes = Traducoes.all
  end
 end
-
-#Adiciona a classe Traducoes.controller para a aplicação. 
-#Herda a classe ApplicationController, ou seja, a classe que eu criei torna-se uma extensão da classe ApplicationController, que o rails considera como padrão
