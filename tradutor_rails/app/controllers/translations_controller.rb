a7387a8965a355b203a0b38a6ec5a30095d6321a
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-06-17 19:19:18 -0300

    Adiciona metodo edit e update

--- tradutor_rails/app/controllers/translations_controller.rb
+++ tradutor_rails/app/controllers/translations_controller.rb
@@ -14,5 +14,33 @@ class TranslationsController < ApplicationController
   end
 
   def show
+    id = params[:id]
+    @tr = Translation.find(id)
   end
- end
+
+  def create
+    @translation = Translation.new(translation_params)
+
+    @translation.save
+    redirect_to @translation
+  end
+
+  def edit
+    @translation = Translation.find(params[:id])
+  end
+
+  def update
+    @translation = Translation.find(params[:id])
+
+    if @translation.update(translation_params)
+      redirect_to @translation
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def translation_params
+      params.require(:translation).permit(:portuguese, :english)
+    end
+end
