a6a47bf0be89fa86f1de0d0ec6f8f61e7d7b1b94
Elizabeth Ramos <elizabethlopesramos@gmail.com>
2017-07-03 15:00:39 -0300

    Adiciona solicitação de tradução

--- tradutor_rails/app/controllers/translation_requests_controller.rb
+++ tradutor_rails/app/controllers/translation_requests_controller.rb
@@ -5,6 +5,19 @@ class TranslationRequestsController <ApplicationController
   end
 
   def create
+    @translation_request = TranslationRequest.new(translation_request_params)
+    @translation_request.save
+
+    redirect_to @translation_request
   end
 
+  def show
+  end
+
+end
+
+
+private
+def translation_request_params
+  params.require(:translation_request).permit(:text)
 end
